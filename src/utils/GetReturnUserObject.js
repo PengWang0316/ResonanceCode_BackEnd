89087a0adb7c89e6e5c52a6de8e3bc8d5d012649
Kevin W <rocwang0316@gmail.com>
2018-05-22 16:34:36 -0700

    Separated the GetReturnUserObject.js from the NormalRouter.js

---
+++ src/utils/GetReturnUserObject.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config(); // Loading .env to process.env
+
+const { NORMAL_ROLE } = process.env;
+
+/** Get information from database's return and sign the user Object with jwt.
+  * @param {object} user comes from database.
+  * @return {object} return an object that contains jwt message and formated user object.
+*/
+module.exports = user => {
+  const returnUser = Object.assign({
+    isAuth: true, role: user.role || NORMAL_ROLE
+  }, user);
+  return { jwt: jwt.sign(returnUser, process.env.JWT_SECERT), user: returnUser };
+};
