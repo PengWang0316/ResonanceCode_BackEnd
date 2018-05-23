928cc382544b3b99b7ae3515fb03ab64d7823c89
Kevin W <rocwang0316@gmail.com>
2018-05-22 17:25:06 -0700

    Formated PutUpdateSettingCoinMode.js

--- src/routers/functions/PutUpdateSettingCoinMode.js
+++ src/routers/functions/PutUpdateSettingCoinMode.js
@@ -4,5 +4,7 @@ const mongodb = require('../../MongoDB');
 const getReturnUserObject = require('../../utils/GetReturnUserObject');
 
 module.exports = (req, res) =>
-  mongodb.updateUser(verifyJWT({ message: req.body.jwtMessage, res })._id, { 'settings.coinMode': req.body.coinMode })
-    .then(result => res.json(getReturnUserObject(result.value))).catch(err => logger.error(err));
+  mongodb.updateUser(
+    verifyJWT({ message: req.body.jwtMessage, res })._id,
+    { 'settings.coinMode': req.body.coinMode }
+  ).then(result => res.json(getReturnUserObject(result.value))).catch(err => logger.error(err));
