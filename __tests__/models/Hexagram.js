eed1890345ded1460d225d12c80def0a211b8a75
Kevin Wang <rocwang0316@gmail.com>
2018-11-10 22:35:22 -0800

    Added the Hexagram model.

---
+++ __tests__/models/Hexagram.js
@@ -0,0 +1,97 @@
+import { ObjectId } from 'mongodb';
+
+import Hexagram from '../../src/models/Hexagram';
+
+const mockFind = jest.fn();
+const mockCollection = jest.fn().mockReturnValue({ find: mockFind });
+jest.mock('../../src/MongoDBHelper', () => ({
+  promiseFindResult: jest.fn().mockImplementation(callback => callback({
+    collection: mockCollection,
+  })),
+}));
+
+describe('Hexagram model', () => {
+  test('fetchHexagrams with empty query', () => {
+    Hexagram.fetchHexagrams({});
+
+    expect(mockCollection).toHaveBeenCalledTimes(1);
+    expect(mockCollection).toHaveBeenLastCalledWith('hexagrams');
+    expect(mockFind).toHaveBeenCalledTimes(1);
+    expect(mockFind).toHaveBeenLastCalledWith({});
+  });
+
+  test('fetchHexagrams with upperId query', () => {
+    Hexagram.fetchHexagrams({ upperId: '0' });
+
+    expect(mockCollection).toHaveBeenCalledTimes(2);
+    expect(mockCollection).toHaveBeenLastCalledWith('hexagrams');
+    expect(mockFind).toHaveBeenCalledTimes(2);
+    expect(mockFind).toHaveBeenLastCalledWith({});
+
+    Hexagram.fetchHexagrams({ upperId: '5b182e9138dbb7258cc39547' });
+    expect(mockCollection).toHaveBeenCalledTimes(3);
+    expect(mockCollection).toHaveBeenLastCalledWith('hexagrams');
+    expect(mockFind).toHaveBeenCalledTimes(3);
+    expect(mockFind).toHaveBeenLastCalledWith({ upper_trigrams_id: new ObjectId('5b182e9138dbb7258cc39547') });
+  });
+
+  test('fetchHexagrams with lowerId query', () => {
+    Hexagram.fetchHexagrams({ lowerId: '0' });
+
+    expect(mockCollection).toHaveBeenCalledTimes(4);
+    expect(mockCollection).toHaveBeenLastCalledWith('hexagrams');
+    expect(mockFind).toHaveBeenCalledTimes(4);
+    expect(mockFind).toHaveBeenLastCalledWith({});
+
+    Hexagram.fetchHexagrams({ lowerId: '5b182e9138dbb7258cc39547' });
+    expect(mockCollection).toHaveBeenCalledTimes(5);
+    expect(mockCollection).toHaveBeenLastCalledWith('hexagrams');
+    expect(mockFind).toHaveBeenCalledTimes(5);
+    expect(mockFind).toHaveBeenLastCalledWith({ lower_trigrams_id: new ObjectId('5b182e9138dbb7258cc39547') });
+  });
+
+  test('fetchHexagrams with line13Id query', () => {
+    Hexagram.fetchHexagrams({ line13Id: '0' });
+
+    expect(mockCollection).toHaveBeenCalledTimes(6);
+    expect(mockCollection).toHaveBeenLastCalledWith('hexagrams');
+    expect(mockFind).toHaveBeenCalledTimes(6);
+    expect(mockFind).toHaveBeenLastCalledWith({});
+
+    Hexagram.fetchHexagrams({ line13Id: '5b182e9138dbb7258cc39547' });
+    expect(mockCollection).toHaveBeenCalledTimes(7);
+    expect(mockCollection).toHaveBeenLastCalledWith('hexagrams');
+    expect(mockFind).toHaveBeenCalledTimes(7);
+    expect(mockFind).toHaveBeenLastCalledWith({ line_13_id: new ObjectId('5b182e9138dbb7258cc39547') });
+  });
+
+  test('fetchHexagrams with line25Id query', () => {
+    Hexagram.fetchHexagrams({ line25Id: '0' });
+
+    expect(mockCollection).toHaveBeenCalledTimes(8);
+    expect(mockCollection).toHaveBeenLastCalledWith('hexagrams');
+    expect(mockFind).toHaveBeenCalledTimes(8);
+    expect(mockFind).toHaveBeenLastCalledWith({});
+
+    Hexagram.fetchHexagrams({ line25Id: '5b182e9138dbb7258cc39547' });
+    expect(mockCollection).toHaveBeenCalledTimes(9);
+    expect(mockCollection).toHaveBeenLastCalledWith('hexagrams');
+    expect(mockFind).toHaveBeenCalledTimes(9);
+    expect(mockFind).toHaveBeenLastCalledWith({ line_25_id: new ObjectId('5b182e9138dbb7258cc39547') });
+  });
+
+  test('fetchHexagrams with line46Id query', () => {
+    Hexagram.fetchHexagrams({ line46Id: '0' });
+
+    expect(mockCollection).toHaveBeenCalledTimes(10);
+    expect(mockCollection).toHaveBeenLastCalledWith('hexagrams');
+    expect(mockFind).toHaveBeenCalledTimes(10);
+    expect(mockFind).toHaveBeenLastCalledWith({});
+
+    Hexagram.fetchHexagrams({ line46Id: '5b182e9138dbb7258cc39547' });
+    expect(mockCollection).toHaveBeenCalledTimes(11);
+    expect(mockCollection).toHaveBeenLastCalledWith('hexagrams');
+    expect(mockFind).toHaveBeenCalledTimes(11);
+    expect(mockFind).toHaveBeenLastCalledWith({ line_46_id: new ObjectId('5b182e9138dbb7258cc39547') });
+  });
+});
---
+++ src/models/Hexagram.js
@@ -0,0 +1,23 @@
+const { ObjectId } = require('mongodb');
+
+const { promiseFindResult } = require('../MongoDBHelper');
+
+const COLLECTION_HEXAGRAMS = 'hexagrams';
+
+/** working with method below in order to transfer the query object to the correct format.
+  * @param {object} query is an object that contains the filter information for the Hexagram.
+  * @returns {object} Return the query object that can be used in the database.
+*/
+function getHexagramsQueryObject(query) {
+  const queryObject = {};
+  if (query.upperId && query.upperId !== '0') queryObject.upper_trigrams_id = new ObjectId(query.upperId);
+  if (query.lowerId && query.lowerId !== '0') queryObject.lower_trigrams_id = new ObjectId(query.lowerId);
+  if (query.line13Id && query.line13Id !== '0') queryObject.line_13_id = new ObjectId(query.line13Id);
+  if (query.line25Id && query.line25Id !== '0') queryObject.line_25_id = new ObjectId(query.line25Id);
+  if (query.line46Id && query.line46Id !== '0') queryObject.line_46_id = new ObjectId(query.line46Id);
+  return queryObject;
+}
+
+/*  fetch hexagrams  */
+exports.fetchHexagrams = query => promiseFindResult(db => db.collection(COLLECTION_HEXAGRAMS)
+  .find(getHexagramsQueryObject(query)));
