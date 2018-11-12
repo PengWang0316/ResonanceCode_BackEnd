14250a8c728a60d4bff5468035a8861bb798425e
Kevin Wang <rocwang0316@gmail.com>
2018-11-12 15:51:54 -0800

    Moved the fetchRecentReadings and two more helper functions to the Reading model. This function will be refactored due to the bad performance. The Redux state will be use to supply the hexagram data

--- src/MongoDB.js
+++ src/MongoDB.js
@@ -172,54 +172,57 @@ exports.createReading = reading => new Promise((resolve, reject) =>
   connectToDb(db => db.collection(COLLECTION_READINGS)
     .insert(reading, (err, response) => resolve(response.ops[0]))));
 
-/* Working with method below to execute the callback function when all hexagram are fetched. */
-const checkHexagramImageReadAndCallback = (checkNumber, targetNumber, callback, result) => {
-  if (checkNumber === targetNumber) callback(result);
-};
+// /* Working with method below to execute the callback function when all hexagram are fetched. */
+// Moved to the Reading model
+// const checkHexagramImageReadAndCallback = (checkNumber, targetNumber, callback, result) => {
+//   if (checkNumber === targetNumber) callback(result);
+// };
 
-/** This method is using to find hexagram information for readings
-  * @param {array} readings is an array that has reading objects.
-  * @param {function} callback is a function will be transfered to anther function.
-  * @return {null} No return.
- */
-const findHexagramImages = (readings, callback) => {
-  let checkNumber = 0;
-  const targetNumber = readings.length * 2;
-  // Making a copy for the readings. So, the code below is safe when change reading in the forEach function.
-  const copyReadings = [...readings];
-  connectToDb((db) => {
-    copyReadings.forEach(reading => {
-      db.collection(COLLECTION_HEXAGRAMS)
-        .find({ img_arr: reading.hexagram_arr_1 }).next((err, imgInfo) => {
-          reading.img1Info = imgInfo;
-          checkNumber += 1;
-          checkHexagramImageReadAndCallback(checkNumber, targetNumber, callback, copyReadings);
-        });
-      db.collection(COLLECTION_HEXAGRAMS)
-        .find({ img_arr: reading.hexagram_arr_2 }).next((err, imgInfo) => {
-          reading.img2Info = imgInfo;
-          checkNumber += 1;
-          checkHexagramImageReadAndCallback(checkNumber, targetNumber, callback, copyReadings);
-        });
-    });
-  });
-};
+// /** This method is using to find hexagram information for readings
+//   * @param {array} readings is an array that has reading objects.
+//   * @param {function} callback is a function will be transfered to anther function.
+//   * @return {null} No return.
+//  */
+// Moved to the Reading model
+// const findHexagramImages = (readings, callback) => {
+//   let checkNumber = 0;
+//   const targetNumber = readings.length * 2;
+//   // Making a copy for the readings. So, the code below is safe when change reading in the forEach function.
+//   const copyReadings = [...readings];
+//   connectToDb((db) => {
+//     copyReadings.forEach(reading => {
+//       db.collection(COLLECTION_HEXAGRAMS)
+//         .find({ img_arr: reading.hexagram_arr_1 }).next((err, imgInfo) => {
+//           reading.img1Info = imgInfo;
+//           checkNumber += 1;
+//           checkHexagramImageReadAndCallback(checkNumber, targetNumber, callback, copyReadings);
+//         });
+//       db.collection(COLLECTION_HEXAGRAMS)
+//         .find({ img_arr: reading.hexagram_arr_2 }).next((err, imgInfo) => {
+//           reading.img2Info = imgInfo;
+//           checkNumber += 1;
+//           checkHexagramImageReadAndCallback(checkNumber, targetNumber, callback, copyReadings);
+//         });
+//     });
+//   });
+// };
 
-/*  Get readings  */
-exports.getRecentReadings = (pageNumber, numberPerpage, userId) =>
-  new Promise((resolve, reject) => {
-    connectToDb(db => {
-      db.collection(COLLECTION_READINGS)
-        .find(userId ? { user_id: userId } : {})
-        .sort({ date: -1 }).limit(numberPerpage * 1)
-        .skip(pageNumber * numberPerpage)
-        .toArray((err, result) => {
-          if (err) logger.error('Something goes worry: ', err);
-          if (result.length !== 0) findHexagramImages(result, backResult => resolve(backResult));
-          else resolve(result);
-        });
-    });
-  });
+// /*  Get readings  */
+// Moved to the Reading model
+// exports.getRecentReadings = (pageNumber, numberPerpage, userId) =>
+//   new Promise((resolve, reject) => {
+//     connectToDb(db => {
+//       db.collection(COLLECTION_READINGS)
+//         .find(userId ? { user_id: userId } : {})
+//         .sort({ date: -1 }).limit(numberPerpage * 1)
+//         .skip(pageNumber * numberPerpage)
+//         .toArray((err, result) => {
+//           if (err) logger.error('Something goes worry: ', err);
+//           if (result.length !== 0) findHexagramImages(result, backResult => resolve(backResult));
+//           else resolve(result);
+//         });
+//     });
+//   });
 
 /** Working with method below to search readings based on the hexagram.
   * @param {object} query is an object that has reading's information that a user wants to search.
--- src/models/Reading.js
+++ src/models/Reading.js
@@ -1,8 +1,10 @@
 const { ObjectId } = require('mongodb');
+const logger = require('../utils/Logger');
 
-const { promiseInsertResult, promiseFindResult } = require('../MongoDBHelper');
+const { promiseInsertResult, promiseFindResult, getDB } = require('../MongoDBHelper');
 
 const COLLECTION_READINGS = 'readings';
+const COLLECTION_HEXAGRAMS = 'hexagrams';
 
 /*  Delete a reading  */
 exports.deleteReading = ({ readingId, userId }) => promiseInsertResult(db => db
@@ -26,3 +28,50 @@ exports.fetchJournalList = queryObject => promiseFindResult(db => {
   if (queryObject.userId) query.user_id = queryObject.userId;
   return db.collection(COLLECTION_READINGS).find(query, { journal_entries: 1 });
 });
+
+/* ************  The code below is not tested since it will be refactored with Redux to improve the performence. ************** */
+/* Working with method below to execute the callback function when all hexagram are fetched. */
+const checkHexagramImageReadAndCallback = (checkNumber, targetNumber, callback, result) => {
+  if (checkNumber === targetNumber) callback(result);
+};
+
+/** This method is using to find hexagram information for readings
+  * @param {array} readings is an array that has reading objects.
+  * @param {function} callback is a function will be transfered to anther function.
+  * @return {null} No return.
+ */
+const findHexagramImages = (readings, callback) => {
+  let checkNumber = 0;
+  const targetNumber = readings.length * 2;
+  // Making a copy for the readings. So, the code below is safe when change reading in the forEach function.
+  const copyReadings = [...readings];
+  copyReadings.forEach(reading => {
+    getDB().collection(COLLECTION_HEXAGRAMS)
+      .find({ img_arr: reading.hexagram_arr_1 }).next((err, imgInfo) => {
+        reading.img1Info = imgInfo;
+        checkNumber += 1;
+        checkHexagramImageReadAndCallback(checkNumber, targetNumber, callback, copyReadings);
+      });
+    getDB().collection(COLLECTION_HEXAGRAMS)
+      .find({ img_arr: reading.hexagram_arr_2 }).next((err, imgInfo) => {
+        reading.img2Info = imgInfo;
+        checkNumber += 1;
+        checkHexagramImageReadAndCallback(checkNumber, targetNumber, callback, copyReadings);
+      });
+  });
+};
+
+/*  Get readings  */
+exports.fetchRecentReadings = (pageNumber, numberPerpage, userId) => new Promise((resolve, reject) => {
+  getDB().collection(COLLECTION_READINGS)
+    .find(userId ? { user_id: userId } : {})
+    .sort({ date: -1 })
+    .limit(numberPerpage * 1)
+    .skip(pageNumber * numberPerpage)
+    .toArray((err, result) => {
+      if (err) logger.error('Reading getRecentReadings something goes worry: ', err);
+      if (result.length !== 0) findHexagramImages(result, backResult => resolve(backResult));
+      else resolve(result);
+    });
+});
+/* ************  The code above is not tested since it will be refactored with Redux to improve the performence. ************** */
